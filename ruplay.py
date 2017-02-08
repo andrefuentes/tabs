ed14f2c1a9eeb611c391a645706ad23f1d3018c8
andrefuentes <andrefuentes166@gmail.com>
2017-02-08 08:35:43 -0600

    1.0

--- ruplay.py
+++ ruplay.py
@@ -1,1 +1,6 @@
-import 
+from tabs import Tabs
+from navegador import Navegador
+ opc=0
+ print ("""1.crear tab    2. cambiar URl del tab    4.cerrar todos los tabs 
+ 	4.cargar todos los tabs 5.mostrar mis tabs 6.guardar mis tabs 7.salir  """)
+if opc==1
